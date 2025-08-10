3d5ca1a9c247491448668b298250321ee62694d5
HHK5253 <hatimkutarwadliwala@gmail.com>
2025-08-10 11:41:20 +0530

    backend resolved 2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -26,7 +26,14 @@ SECRET_KEY = "django-insecure-8(^-e9gp&i)(dhlss7r7p=mdo4mc(+ync4wi9+-s+k=r^uib=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0,flexora-ri89.onrender.com").split(",")
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "flexora-ri89.onrender.com"
+]
+
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0,flexora-ri89.onrender.com").split(",")
 
 
 
